2c91c4e283839a613267410f7b0a115451f44734
Cherry <manyberry38@gamil.com>
2024-02-06 02:12:30 +0200

    create anim and scrol from nav to section

--- script.js
+++ script.js
@@ -1,6 +1,48 @@
 const tabs = document.querySelectorAll(".tabs-tab");
 const contents = document.querySelectorAll(".swiper-slide");
+const bg = document.querySelectorAll(".skills__item");
 
+// header
+window.addEventListener("scroll", function () {
+    scrollY > 0
+        ? document.querySelector(".header").classList.add("scroll")
+        : document.querySelector(".header").classList.remove("scroll");
+});
+
+//animation
+for (let i = 0; i < bg.length; i++) {
+    window.addEventListener("mousemove", function (e) {
+        let x = e.clientX / window.innerWidth;
+        let y = e.clientY / window.innerHeight;
+        // let r = Math.random() * 22;
+        // console.log(r);
+        bg[i].style.transform =
+            "translate(-" + x * 40 + "px, -" + y * 40 + "px)";
+    });
+}
+//navigation
+
+document.querySelectorAll('a[href^="#"').forEach(link => {
+
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        let href = this.getAttribute('href').substring(1);
+
+        const scrollTarget = document.getElementById(href);
+
+        const topOffset = 70; 
+        const elementPosition = scrollTarget.getBoundingClientRect().top;
+        const offsetPosition = elementPosition - topOffset;
+
+        window.scrollBy({
+            top: offsetPosition,
+            behavior: 'smooth'
+        });
+    });
+});
+
+//swiper
 const swiper = new Swiper(".content-swiper", {
     // Optional parameters
 
@@ -22,6 +64,7 @@ const swiper = new Swiper(".content-swiper", {
     spaceBetween: 20,
 });
 
+//tabs
 const showContent = (tab) => {
     [...contents].forEach((content) => {
         const contentStyle = content.dataset.name;
